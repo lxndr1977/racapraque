5afce07d3f650a41414c47f83a9ec7d6b4e1fd1a
Alexandre Pereira <pereira.aleandre@gmail.com>
2025-09-14 20:25:28 -0300

    Adiciona um modal após o apadrinhamento com o link de pagamento. Adiciona um lembrete de pagamento no formulário

--- resources/views/partials/animal/sponsorship-confirmation-modal.blade.php
+++ resources/views/partials/animal/sponsorship-confirmation-modal.blade.php
@@ -17,7 +17,7 @@
       x-transition:enter-end="opacity-100 scale-100"
       x-transition:leave="transition ease-in duration-200"
       x-transition:leave-start="opacity-100 scale-100"
-      x-transition:leave-end="opacity-0 scale-95">
+      x-transition:leave-end="opacity-0 scale-95"> 
 
       <!-- Modal Header -->
       <div class="flex justify-between items-center p-4 border-b border-zinc-200">
